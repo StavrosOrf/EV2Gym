Fix power setpoint calculation and adjust plot font sizes

evsim/loaders.py
@@ -40,7 +40,8 @@ def load_power_setpoints(env,randomly):
 
     if randomly:
         inverse_prices = 1/abs(env.charge_prices[0,:]+0.001)
-        return power_setpoints*(inverse_prices*env.cs)*np.random.uniform(0.25,0.35,1)
+        # return power_setpoints*(inverse_prices*env.cs)*np.random.uniform(0.25,0.35,1)
+        return power_setpoints*(inverse_prices*env.cs)*np.random.uniform(0.08,0.09,1)
     else:
         raise NotImplementedError('Loading power setpoints from is not implemented yet')
 