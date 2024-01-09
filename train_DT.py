372da0ecdd41b92920a62d8abce42a1d99438e70
stavrosgreece <stayros_1@yahoo.gr>
2024-01-09 13:54:15 +0100

    Updating DT

--- train_DT.py
+++ train_DT.py
@@ -7,12 +7,13 @@ import argparse
 import pickle
 import random
 import sys
+import yaml
 
-from decision_transformer.evaluation.evaluate_episodes import evaluate_episode, evaluate_episode_rtg
-from decision_transformer.models.decision_transformer import DecisionTransformer
-from decision_transformer.models.mlp_bc import MLPBCModel
-from decision_transformer.training.act_trainer import ActTrainer
-from decision_transformer.training.seq_trainer import SequenceTrainer
+from baselines.DT.evaluation.evaluate_episodes import evaluate_episode, evaluate_episode_rtg
+from baselines.DT.models.decision_transformer import DecisionTransformer
+from baselines.DT.models.mlp_bc import MLPBCModel
+from baselines.DT.training.act_trainer import ActTrainer
+from baselines.DT.training.seq_trainer import SequenceTrainer
 
 from EVsSimulator import ev_city
 
@@ -57,25 +58,22 @@ def experiment(
     if model_type == 'bc':
         # since BC ignores target, no need for different evaluations
         env_targets = env_targets[:1]
-        
-    n_transformers = 1
-    number_of_charging_stations = 10
-    steps = 288  # 288 steps = 1 day with 5 minutes per step
-    timescale = 5  # (5 minutes per step)
-    # n_test_cycles = args.n_test_cycles
     
-    replay_path = None
+    config = yaml.load(open(args.config_file, 'r'), Loader=yaml.FullLoader)
+    number_of_charging_stations = config["number_of_charging_stations"]
+    n_transformers = config["number_of_transformers"]
+    steps = config["simulation_length"]
+    # timescale = config["timescale"]
+
+    # replay_path = None
 
     args.env = 'evcity-v1'
 
-    env = ev_city.EVCity(cs=number_of_charging_stations,
-                         number_of_transformers=n_transformers,
-                         load_from_replay_path=replay_path,
+    env = ev_city.EVCity(config_file=args.config_file,
                          generate_rnd_game=True,
-                         simulation_length=steps,
-                         timescale=timescale,
                          save_plots=False,
-                         save_replay=False,)
+                         save_replay=False,
+                         )
 
     state_dim = env.observation_space.shape[0]
     act_dim = env.action_space.shape[0]
@@ -97,12 +95,7 @@ def experiment(
     else:
         raise NotImplementedError("Dataset not found")
 
-    #split dataset path to get group name
-    number_of_charging_stations = dataset_path.split("_")[1]
-    n_transformers = dataset_path.split("_")[3]        
-    timesteps = int(dataset_path.split("_")[5])
-    max_ep_len = int(timesteps)
-    timescale = int(dataset_path.split("_")[7])
+    max_ep_len = steps
     g_name = variant['group_name']
 
     group_name = f'{g_name}_DT_{number_of_charging_stations}cs_{n_transformers}tr'    
@@ -241,6 +234,7 @@ def experiment(
                         state_std=state_std,
                         device=device,
                         n_test_episodes=num_eval_episodes,
+                        config_file=args.config_file,
                     )
                 else:
                     ret, length = evaluate_episode(
@@ -255,15 +249,9 @@ def experiment(
                         state_std=state_std,
                         device=device,
                     )
-                # returns.append(ret)
-                # lengths.append(length)
+
             return stats
-        # {
-        #         f'target_{target_rew}_return_mean': np.mean(returns),
-        #         f'target_{target_rew}_return_std': np.std(returns),
-        #         f'target_{target_rew}_length_mean': np.mean(lengths),
-        #         f'target_{target_rew}_length_std': np.std(lengths),
-        #     }
+
         return fn
 
     if model_type == 'dt':
