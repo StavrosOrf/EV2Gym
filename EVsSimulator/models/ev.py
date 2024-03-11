Fix battery degradation calculation and update evaluation algorithms

EVsSimulator/models/ev.py
@@ -364,8 +364,8 @@ class EV():
         b_cap_ah = 2.05  # ah
         b_cap_kwh = 78  # kwh
 
-        d_dist = 11120  # km
-        b_age = 365  # days
+        d_dist = 15000  # km
+        b_age = 2*365  # days
         G = 0.186  # kwh/km
 
         # Age of the battery in days
@@ -377,7 +377,7 @@ class EV():
         theta = 298.15  # Kelvin
         k = 0.8263  # Volts
 
-        v_min = 3.15  # Volts
+        v_min = 3.3324  # Volts
         self.historic_soc.append(self.get_soc()) # Add the final soc to the historic soc
         avg_soc = np.mean(self.historic_soc) 
         v_avg = v_min + k * avg_soc
@@ -398,7 +398,7 @@ class EV():
         delta_DoD = 2 * abs(avg_filtered_soc.repeat(len( filtered_historic_soc)) - filtered_historic_soc).mean()
         # print(f'delta_DoD: {delta_DoD}')
         v_half_soc = v_min + k * 0.5
-        beta = z0 * (v_half_soc / math.sqrt(2) - z1)**2 + z2 + z3 * delta_DoD        
+        beta = z0 * (v_half_soc - z1)**2 + z2 + z3 * delta_DoD        
 
         Q_sim = (self.abs_total_energy_exchanged / b_cap_kwh) * b_cap_ah
 

tests/plot_exec_time.py
@@ -8,6 +8,8 @@ import numpy as np
 
 paths = ['results/eval_6cs_3tr_V2G_MPC_5_algos_20_exp_2024_03_07_610105',
          #  'results/eval_48cs_3tr_V2G_MPC_5_algos_5_exp_2024_03_07_166424',
+         'results/eval_30cs_3tr_V2G_MPC_5_algos_2_exp_2024_03_08_380121',
+         'results/eval_18cs_3tr_V2G_MPC_5_algos_2_exp_2024_03_08_704132',
          'results/eval_24cs_3tr_V2G_MPC_5_algos_5_exp_2024_03_07_055202',
          'results/eval_12cs_3tr_V2G_MPC_5_algos_20_exp_2024_03_07_523345']
 
@@ -49,6 +51,10 @@ results_all['Algorithm'] = results_all['Algorithm'].replace({'ChargeAsFastAsPoss
                                                              'ChargeAsFastAsPossible': 'AFAP',
                                                              'ChargeAsLateAsPossible': 'ALAP', })
 print(results_all.columns)
+#drop rows where algorithm = 'ChargeAsFastAsPossibleToDesiredCapacity'
+
+results_all = results_all[results_all['Algorithm'] != 'AFAP']
+
 
 # plot in the same plot the execution time of the algorithms for different cs numbers
 sns.set_theme(style="whitegrid")
@@ -70,7 +76,7 @@ ax = sns.catplot(x="cs_number",
                  native_scale=True,
                  kind="point",
                  errorbar="se",
-                 order=[6, 12, 24],
+                 order=[6, 12,18,24,30],
                  height=6,
                  aspect=.75,
                  alpha=0.7,
@@ -79,8 +85,8 @@ ax = sns.catplot(x="cs_number",
 # logarithmic scale
 # ax.set(yscale="log")
 # use xticks to match the cs_number and real number of charging stations
-ax.set(xticks=np.arange(6,25, step=6),
-       xticklabels=np.arange(6, 25, step=6))
+ax.set(xticks=np.arange(6,31, step=6),
+       xticklabels=np.arange(6, 31, step=6))
 # set labels
 #increase font size
 ax.set_xticklabels(fontsize=22)
@@ -99,7 +105,7 @@ ax = sns.catplot(x="cs_number",
                  hue="Algorithm",
                  data=results_all,
                  kind="point",
-                 order=[6, 12, 24],
+                 order=[6, 12,18,24,30],
                  height=6,
                  aspect=.75,
                  legend_out=False,)

tests/test_degradation.py
@@ -26,7 +26,7 @@ def battery_degradation_test():
 
     amps_list = np.arange(8, 56, 1)
     amps_list = [56]
-    required_energy_list = np.arange(0, 50, 0.5)
+    required_energy_list = np.arange(0, 50, 1)
 
     calendar_degradation = np.zeros(
         (len(amps_list), len(required_energy_list)))
@@ -91,7 +91,7 @@ def battery_degradation_test():
         e1 = 23.75e6
         e2 = 6976
 
-        b_age = 365  # days        
+        b_age = 2*365  # days        
 
         # Age of the battery in days
         T_acc = b_age
@@ -101,7 +101,7 @@ def battery_degradation_test():
 
         theta = 298.15  # Kelvin
         k = 0.8263  # Volts
-        v_min = 3.15  # Volts
+        v_min = 3.3324  # Volts
         
         v_avg = v_min + k * avg_soc
 
@@ -136,7 +136,7 @@ def battery_degradation_test():
     
     # move scilimits to the left
     # ax1.yaxis.major.formatter._useMathText = True
-    ax1.set_ylim(0,8)
+    # ax1.set_ylim(0,2)
     
     
     ax1.legend(["Calendar deg."],loc='upper left',bbox_to_anchor=(0, 0.7),)
@@ -164,7 +164,7 @@ def battery_degradation_test():
     #grid lines
     ax1.grid()
     ax2.grid()
-    plt.show(   )
+    plt.show()
     
     
     exit()