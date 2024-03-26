947890c0d49e378554ed1435b30d37fa9a205240
Stavros Orfanoudakis <stayros_1@yahoo.gr>
2024-03-26 15:51:44 +0100

    Working on DT

--- ev2gym/scripts/generate_trajectories.py
+++ ev2gym/scripts/generate_trajectories.py
@@ -33,42 +33,25 @@ if __name__ == "__main__":
     n_transformers = args.transformers
     number_of_charging_stations = args.cs
     steps = args.steps  # 288 steps = 1 day with 5 minutes per step
-    timescale = args.timescale  # (5 minutes per step)    
+    timescale = args.timescale  # (5 minutes per step)
     n_trajectories = args.n_trajectories
 
-    gym.register(id='evcity-v0', entry_point='gym_env.ev_city:EVCity')
-
-    env = ev2gym_env.EV2Gym(cs=number_of_charging_stations,
-                         number_of_transformers=n_transformers,
-                         load_from_replay_path=None,
-                         generate_rnd_game=True,
-                         simulation_length=steps,
-                         timescale=timescale,
-                         save_plots=False,
-                         save_replay=False,)
+    env = ev2gym_env.EV2Gym(config_file=args.config_file,
+                            generate_rnd_game=True,
+                            state_function
+                            )
 
     trajectories = []
-
-    # Set random seed for all used libraries where possible
-    # env.seed(args.seed)
-    # torch.manual_seed(args.seed)
-    # np.random.seed(args.seed)
-    # random.seed(args.seed)
 
     if torch.cuda.is_available():
         torch.cuda.manual_seed(args.seed)
         torch.backends.cudnn.deterministic = True
         torch.backends.cudnn.benchmark = False
 
-    # Define and build DDPG agent
-    hidden_size = tuple(args.hidden_size)
-
-    if args.dataset not in ["ddpg","random","optimal"]:
+    if args.dataset not in ["ddpg", "random", "optimal"]:
         raise ValueError("Dataset not supported")
-    
+
     trajecotries_type = args.dataset
-
-
 
     file_name = f"{trajecotries_type}_{number_of_charging_stations}_cs_{n_transformers}_tr_{steps}_steps_{timescale}_timescale_{n_trajectories}_trajectories.pkl"
     save_folder_path = f"./trajectories/"
@@ -80,12 +63,7 @@ if __name__ == "__main__":
 
     # Define counters and other variables
     start_step = 0
-    # timestep = start_step
 
-
-        
-
-    timestep = start_step // 10000 + 1
     epoch = 0
 
     for i in range(n_trajectories):
@@ -93,22 +71,21 @@ if __name__ == "__main__":
         trajectory_i = {"observations": [],
                         "actions": [],
                         "rewards": [],
-                        "dones": [] }
+                        "dones": []}
 
         epoch_return = 0
 
         print(f'Trajectory: {i}')
-        state = torch.Tensor([env.reset()]).to(device)       
+        state = torch.Tensor([env.reset()]).to(device)
 
         state = torch.Tensor([env.reset()]).to(device)
         test_reward = 0
         while True:
-            
-            if args.dataset == "random":
-                action = torch.rand(1,env.number_of_ports) #*2-1
 
-            next_state, reward, done, stats = env.step(
-                action.cpu().numpy()[0])
+            if args.dataset == "random":
+                action = torch.rand(1, env.number_of_ports)  # *2-1
+
+            next_state, reward, done, stats = env.step(action)
             test_reward += reward
 
             trajectory_i["observations"].append(state.cpu().numpy()[0])
@@ -121,7 +98,7 @@ if __name__ == "__main__":
 
             if done:
                 break
-        
+
         trajectory_i["observations"] = np.array(trajectory_i["observations"])
         trajectory_i["actions"] = np.array(trajectory_i["actions"])
         trajectory_i["rewards"] = np.array(trajectory_i["rewards"])
