b3905f19036bd73651bcada1e7daa26f25d3470a
Stavros Orfanoudakis <stayros_1@yahoo.gr>
2024-10-16 00:21:43 +0200

    Update .gitignore and grid.py

--- ev2gym/models/grid.py
+++ ev2gym/models/grid.py
@@ -1,5 +1,5 @@
 # This file implements functions that simulate the grid of the simulations using PandaPower
-#### NOT used yet, it is in development phase!!!!!!!!!!!!!!
+# NOT used yet, it is in development phase!!!!!!!!!!!!!!
 from pandapower.plotting.plotly import pf_res_plotly
 import pandapower as pp
 import numpy as np
@@ -12,78 +12,83 @@ set_mapbox_token(
 
 class Grid:
 
-    def __init__(self, charging_stations, seed=9, case="default"):
+    def __init__(self, case="kerber_landnetz_freileitung_1"):
 
-        self.charging_stations = charging_stations
         # Load the grid case
-        if case == "default":
-            self.net = pp.networks.create_cigre_network_mv(with_der='pv_wind')
+        if case == "kerber_dorfnetz":
+            self.net = pp.networks.create_kerber_dorfnetz()
+            load_bus_name = "loadbus"
+            
+        elif case == "kerber_landnetz_freileitung_2":
+            self.net = pp.networks.create_kerber_landnetz_freileitung_2()
+            load_bus_name = "bus"
+                        
         else:
             raise NotImplementedError
 
-        self.seed = seed
-        np.random.seed(seed)
-
-        # Initialize the Charging Stations
+        # add charging stations(storage units) and PV to every load (house) bus
         for i in range(len(self.net.bus)):
-            # Add a Storage Unit to each bus of the grid except the external grid bus
-            if i not in self.net.ext_grid.bus.values:
+            # print(self.net.bus.loc[i])
+            
+            if load_bus_name in self.net.bus.at[i, 'name']:
                 pp.create_storage(self.net,
                                   bus=i,
                                   p_mw=0,
-                                  max_e_mwh=1000,
-                                  q_mvar=0,  type="ChargingStation")
-        # Initialize the charging stations conneted bus
-        # get charging_stations random integers excluding the integers in self.net.ext_grid.bus.values
-        self.charging_stations_buses = np.random.choice(
-            [i for i in range(len(self.net.bus)) if i not in self.net.ext_grid.bus.values],
-            charging_stations, replace=True)
+                                  max_e_mwh=100,  # this parameter is not used in the PF calculation
+                                  q_mvar=0,
+                                  type="ChargingStation")
 
-    def _get_grid_actions(self,actions):
+                pp.create_sgen(self.net,
+                               bus=i,
+                               p_mw=0,
+                               q_mvar=0,
+                               type="PV")
+
+    def _get_grid_actions(self, actions):
         # Transform ations to the format (number_of_ports,1) -> (bus, p_mw)
 
         return actions
-    
+
     def step(self, actions):
 
         actions = self._get_grid_actions(actions)
 
         # reset CS actions
         for i in range(len(self.net.storage)):
-            self.net.storage.p_mw[i] = 0
+            self.net.storage.at[i, 'p_mw'] = 0
 
         # actions are in the format (bus, p_mw)
         assert (len(actions) == self.charging_stations)
-        for action in actions:
-            self.net.storage.p_mw[action[0]] += action[1]
+        # for action in actions:
+        #     self.net.storage.p_mw[action[0]] += action[1]
 
-        std = 0.1
+        std = 0.01
         # TODO: replace with REAL load profiles
-        self.net.load.p_mw = np.random.normal(
-            self.net.load.p_mw, std, (len(self.net.load)))
-        self.net.load.q_mvar = np.random.normal(
-            self.net.load.q_mvar, std, (len(self.net.load)))
+        # self.net.load.p_mw = np.random.normal(
+        #     self.net.load.p_mw, std, (len(self.net.load)))
+        # self.net.load.q_mvar = np.random.normal(
+        #     self.net.load.q_mvar, std, (len(self.net.load)))
 
-        # TODO: replace with REAL static generation (pv, wind) profiles
-        self.net.sgen.p_mw = np.random.normal(
-            self.net.sgen.p_mw, std, (len(self.net.sgen)))
-        # self.net.sgen.q_mvar = np.random.normal(self.net.sgen.q_mvar, std, (len(self.net.sgen)))
+        # # TODO: replace with REAL static generation (pv, wind) profiles
+        # self.net.sgen.p_mw = np.random.normal(
+        #     self.net.sgen.p_mw, std, (len(self.net.sgen)))
+        # # self.net.sgen.q_mvar = np.random.normal(self.net.sgen.q_mvar, std, (len(self.net.sgen)))
 
-        # TODO: replace with REAL generation profiles
-        self.net.gen.p_mw = np.random.normal(
-            self.net.gen.p_mw, std, (len(self.net.gen)))
-        self.net.gen.vm_pu = np.random.normal(
-            self.net.gen.vm_pu, std, (len(self.net.gen)))
+        # # TODO: replace with REAL generation profiles
+        # self.net.gen.p_mw = np.random.normal(
+        #     self.net.gen.p_mw, std, (len(self.net.gen)))
+        # self.net.gen.vm_pu = np.random.normal(
+        #     self.net.gen.vm_pu, std, (len(self.net.gen)))
 
         # TODO: replace with REAL ext_grid profile
-        self.net.ext_grid.vm_pu = np.random.normal(
-            self.net.ext_grid.vm_pu, std, (len(self.net.ext_grid)))
-        self.net.ext_grid.va_degree = np.random.normal(
-            self.net.ext_grid.va_degree, std, (len(self.net.ext_grid)))
+        # self.net.ext_grid.vm_pu = np.random.normal(
+        #     self.net.ext_grid.vm_pu, std, (len(self.net.ext_grid)))
+        # self.net.ext_grid.va_degree = np.random.normal(
+        #     self.net.ext_grid.va_degree, std, (len(self.net.ext_grid)))
 
         # solve the power flow
         pp.runpp(self.net, numba=True)
-    
+
     def get_grid_state(self):
         '''
         TODO: return the grid state
@@ -120,18 +125,20 @@ class Grid:
 if __name__ == "__main__":
 
     # this is how Grid2op does it https://github.com/rte-france/Grid2Op/blob/master/grid2op/Environment/baseEnv.py#L1093
-    
-    grid = Grid(4)
+
+    grid = Grid(case="kerber_landnetz_freileitung_2")
 
     timeslot_duration = 15  # minutes
     simulation_duration = 100  # * timeslot_duration minutes
 
     for simu_step in range(simulation_duration):
         print(f"Simulation step {simu_step}")
-        actions = [(1, 0.5), (2, 0.1), (2, -0.2), (3, 0.1)]
+        actions = [(1, 0.005), (2, 0.001), (2, -0.002), (3, 0.001)]
+        # actions = [(10,0),(1,0),(2,0),(3,0)]
         grid.step(actions)
         grid.visualize_pf()
         # time.sleep(2)
+        input("Press Enter to continue...")
 
         # print information about the overaloading and the lines and the transformer from here
         # print(grid.get_overloaded_lines())
