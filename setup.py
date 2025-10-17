6a79d9424d40d5b03dc2dd2638db3e349b6aeda8
Stavros Orfanoudakis <stayros_1@yahoo.gr>
2025-10-18 00:17:33 +0200

    Bump version to 2.0.0 in setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='ev2gym',
-    version='1.1.0',
+    version='2.0.0',
     description='A realistic V2G simulator environment',
     author='Stavros Orfanoudakis',
     long_description=long_description,
